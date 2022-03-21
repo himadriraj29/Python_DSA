795c1b20633db2b054c2d2eaa551b73bb57701b5
Himadri <himadriraj29@gmail.com>
2022-03-22 02:44:28 +0530

    .

---
+++ Sorting/union.py
@@ -0,0 +1,38 @@
+def union(arr1,arr2):
+    arr3 = []
+    m = len(arr1)
+    n = len(arr2)
+    i = 0
+    j = 0
+    while i<m and j<n:
+        if i>0 and arr1[i]==arr1[i-1]:
+            i += 1
+            continue
+        if j>0 and arr2[j]==arr2[j-1]:
+            j += 1
+            continue
+        elif arr1[i] < arr2[j]:
+            arr3.append(arr1[i])
+            i += 1
+        elif arr1[i] > arr2[j]:
+            arr3.append(arr2[j])
+            j += 1
+        else:
+            arr3.append(arr1[i])
+            i += 1
+            j += 1
+    while i<m:
+        if i>0 and arr1[i]==arr1[i-1]:
+            i += 1
+            continue
+        arr3.append(arr1[i])
+        i += 1
+    while j<n:
+        if j>0 and arr2[j]==arr2[j-1]:
+            j += 1
+            continue
+        arr3.append(arr2[j])
+        j += 1
+            
+    print(arr3)
+union([4,6,7,9,10,12,14],[4,7,8,9,11,15])
