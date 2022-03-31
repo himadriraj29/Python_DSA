0d6c7fc77e4c32a6e8d53412bc78f552f94f82bb
Himadri <himadriraj29@gmail.com>
2022-04-01 01:02:23 +0530

    prime nos

---
+++ Mathematics/checkPrime.py
@@ -0,0 +1,11 @@
+def prime(n):
+    if n == 1:
+        return False
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return True
+
+n = 15
+ans = prime(n)
+print(ans)
