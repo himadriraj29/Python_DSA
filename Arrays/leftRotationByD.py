df06e5e138c3904321ff083aa5ab9aaec24bda00
Himadri <himadriraj29@gmail.com>
2022-04-06 14:18:11 +0530

    .

--- Arrays/leftRotationByD.py
+++ Arrays/leftRotationByD.py
@@ -1,3 +1,5 @@
+#method 1
+'''
 def rotate(arr,d):
     n = len(arr)
     temp = []
@@ -8,6 +10,24 @@ def rotate(arr,d):
     for i in range(0,d):
         arr[n-d+i] = temp[i]
     return arr
+
+arr = [8,9,6,4,10,7]
+d = 3
+print(rotate([8,9,6,4,10,7],3))
+'''
+#method 2
+def rotate(arr,d):
+    n = len(arr)
+    reverse(arr,0,d-1)
+    reverse(arr,d,n-1)
+    reverse(arr,0,n-1)
+    return arr
+
+def reverse(arr,low,high):
+    while low < high:
+        arr[low],arr[high] = arr[high],arr[low]
+        low += 1
+        high -= 1
 
 arr = [8,9,6,4,10,7]
 d = 3
