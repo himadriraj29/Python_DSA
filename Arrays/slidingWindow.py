e5d1a0283f70a4b4b76d52a3da1362853862a32a
Himadri <himadriraj29@gmail.com>
2022-04-08 19:08:54 +0530

    .

--- Arrays/slidingWindow.py
+++ Arrays/slidingWindow.py
@@ -5,11 +5,11 @@ def window(arr,k):
     for i in range(k):
         currsum += arr[i] 
         maxsum = currsum
-    for j in range(n):
+    for j in range(k,n):
         currsum += arr[j] - arr[j-k]
         maxsum = max(currsum, maxsum)
     return maxsum
-arr = [45,45,45,33,33,21]
+arr = [1,8,30,-5,20,7]
 ans = window(arr,3)
 print(ans)
 
