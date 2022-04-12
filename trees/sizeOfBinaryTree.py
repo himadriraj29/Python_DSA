2831861f8e8687f47c37b51123302ab2c914c269
Himadri <himadriraj29@gmail.com>
2022-04-12 23:13:50 +0530

    .

---
+++ trees/sizeOfBinaryTree.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self,data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+def buildTree():
+    data = int(input())
+    if data == -1:
+        return None
+    node = Node(data)
+    node.left = buildTree()
+    node.right = buildTree()
+    return node
+
+def size(node):
+    if node == None:
+        return 0
+    else:
+        l = size(node.left)
+        r = size(node.right)
+        return l+r+1
+
+
+
+if __name__ == "__main__":
+    root = buildTree()
+    #print(root)
+    '''
+    root = Node(1)
+    root.left = Node(2)
+    root.left.left = Node(8)
+    root.left.right = Node(7)
+    root.right = Node(3)
+    '''
+    ans = size(root)
+    print(ans)
