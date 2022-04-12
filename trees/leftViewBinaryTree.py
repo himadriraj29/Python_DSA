f04652d65194fe6f85c5c490d2fba3561ee67e16
Himadri <himadriraj29@gmail.com>
2022-04-13 01:37:15 +0530

    .

---
+++ trees/leftViewBinaryTree.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self,data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+def buildTree():
+    data = int(input())
+    if data == -1:
+        return None
+    node = Node(data)
+    node.left = buildTree()
+    node.right = buildTree()
+    return node
+
+def leftview(node):
+    A = []
+    s = -1
+    e = -1
+    A.append(node)
+    A.append(None)
+    s += 1
+    e += 2
+    while s < e:
+        curr = A[s]
+        s += 1
+        if curr == None:
+            A.append(None)
+            e += 1
+        else:
+            if curr.left != None:
+                A.append(curr.left)
+                e += 1
+            if curr.right != None:
+                A.append(curr.right)
+                e += 1
+    
+    n = len(A)
+    print(A[0].data)
+    for i in range(1,n-1):
+        if A[i] == None:
+            print(A[i+1].data)
+
+
+
+    return A
+
+
+
+if __name__ == "__main__":
+    root = buildTree()
+    #print(root)
+    '''
+    root = Node(1)
+    root.left = Node(2)
+    root.left.left = Node(8)
+    root.left.right = Node(7)
+    root.right = Node(3)
+    '''
+    ans = leftview(root)
