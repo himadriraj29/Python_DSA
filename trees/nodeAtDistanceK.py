c4000678b50997e71c683e2ef5cddbdb2cd744e5
Himadri <himadriraj29@gmail.com>
2022-04-12 18:00:04 +0530

    .

---
+++ trees/nodeAtDistanceK.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self,data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+def buildTree():
+    data = int(input())
+    if data == -1:
+        return None
+    node = Node(data)
+    node.left = buildTree()
+    node.right = buildTree()
+    return node
+
+def distance(node,k):
+    if node == None:
+        return -1
+    if k == 0:
+        print(node.data)
+    else:
+        distance(node.left, k-1)
+        distance(node.right, k-1)
+
+if __name__ == "__main__":
+    root = buildTree()
+    #print(root)
+    '''
+    root = Node(1)
+    root.left = Node(2)
+    root.left.left = Node(8)
+    root.left.right = Node(7)
+    root.right = Node(3)
+    '''
+    distance(root,2)
+    
