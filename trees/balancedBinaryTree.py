b6c69aadb54519c97d4b4ce7d6f1e5bb3502e3c1
Himadri <himadriraj29@gmail.com>
2022-04-13 03:06:23 +0530

    .

---
+++ trees/balancedBinaryTree.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self,data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+def buildTree():
+    data = int(input())
+    if data == -1:
+        return None
+    node = Node(data)
+    node.left = buildTree()
+    node.right = buildTree()
+    return node
+
+def height(node):
+    if node == None:
+        return 0
+    lh = height(node.left)
+    rh = height(node.right)
+    return max(lh,rh) + 1
+
+def balancedBT(node):
+    if node == None:
+        return True
+    else:
+        l = height(node.left)
+        r = height(node.right)
+
+        if abs(l-r) <= 1 and balancedBT(node.left) and balancedBT(node.right):
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    root = buildTree()
+    #print(root)
+    '''
+    root = Node(1)
+    root.left = Node(2)
+    root.left.left = Node(8)
+    root.left.right = Node(7)
+    root.right = Node(3)
+    '''
+    ans = balancedBT(root)
+    print(ans)
