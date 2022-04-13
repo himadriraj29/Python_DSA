0170120b4f18654066dbbf0ac086ebd84faeed9a
Himadri <himadriraj29@gmail.com>
2022-04-13 19:44:32 +0530

    .

---
+++ Hashing/distinctElements.py
@@ -0,0 +1,11 @@
+def distinct(A):
+    dis = {}
+    for i in A:
+        if i not in dis:
+            dis[i] = 0
+    return dis
+
+
+if __name__ == "__main__":
+    ans = distinct([2,3,1,2,3,4,5,4])
+    print(ans)
