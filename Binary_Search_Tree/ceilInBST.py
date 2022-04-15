fc6f407b5b46203bfbf40975d4a989891036eece
Himadri <himadriraj29@gmail.com>
2022-04-15 20:00:36 +0530

    .

---
+++ Binary_Search_Tree/ceilInBST.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self,data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+def buildTree():
+    data = int(input())
+    if data == -1:
+        return None
+    node = Node(data)
+    node.left = buildTree()
+    node.right = buildTree()
+    return node
+
+def ceilbst(node,key):
+    res = Node(None)
+    while node != None:
+        if node.data == key:
+            return node           
+        elif node.data < key:
+            node = node.right
+        else:
+            res = node
+            node = node.left
+    return res
+
+    
+
+if __name__ == "__main__":
+    root = buildTree()
+    #print(root)
+    '''
+    root = Node(1)
+    root.left = Node(2)
+    root.left.left = Node(8)
+    root.left.right = Node(7)
+    root.right = Node(3)
+    '''
+    ans = ceilbst(root,4)
+    print(ans.data)
+
