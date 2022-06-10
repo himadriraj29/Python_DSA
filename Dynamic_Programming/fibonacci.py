bc4cd6a1eab15e8b18198242d12cd8e51c9be5cf
Himadri <himadriraj29@gmail.com>
2022-06-10 16:51:08 +0530

    DP

---
+++ Dynamic_Programming/fibonacci.py
@@ -0,0 +1,17 @@
+def fiboDP(n):
+    memo = [-1 for i in range(n+1)]
+    #for i in range(n+2):
+        #memo[i].append(-1)
+    if memo[n] == -1:
+        res = 0
+        if n == 0 or n == 1:
+            res = n
+        else:
+            res = fiboDP(n-1) + fiboDP(n-2)
+        memo[n] = res
+    return memo[n]
+    
+
+n = 9
+ans = fiboDP(n)
+print(ans)
